1e91b20f8933975d6db1d3cb6c5094cdbbdb803f
Kim Logoshenko <kim.logoshenko@gmx.de>
2020-06-07 08:14:45 +0200

    v1 adds number on click

---
+++ app.js
@@ -0,0 +1,37 @@
+const grid = document.querySelector(".grid");
+const gridItems = document.querySelectorAll(".gridItem");
+const select = document.querySelector(".select");
+const divs = document.querySelectorAll(".button");
+let selectedNumber = 0;
+
+function displayGrid() {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      let item = document.createElement("div");
+      item.innerHTML = `<div class="gridItem" onclick="displayNumber(this)"></div>`;
+      grid.appendChild(item);
+    }
+  }
+  gridItems.forEach((item) => {
+    item.addEventListener("click", displayNumber(this));
+  });
+}
+
+function handleOnClick(div, number) {
+  selectedNumber = number;
+  divs.forEach((div) => {
+    div.classList.remove("selected");
+  });
+  div.classList.add("selected");
+  console.log(selectedNumber);
+}
+
+function displayNumber(item) {
+  if (selectedNumber === 0) {
+    alert("Please choose a number!");
+  } else {
+    item.innerHTML = `${selectedNumber}`;
+  }
+}
+
+displayGrid();
