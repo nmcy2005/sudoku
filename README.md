9da67b1e947eb6afd797afc45e6dd4cb5f8a4940
Kim Logoshenko <kim.logoshenko@gmx.de>
2020-06-07 08:34:53 +0200

    final version with comments

---
+++ README.md
@@ -0,0 +1,11 @@
+# This is a not finished Sudoku game a friend wanted me to help him add
+
+- ## Basic idea behind that:
+  - You choose a number from the select panel at the bottom
+  - This number will be saved in the variable **selectedNumber**
+  - Then, when you **click** on a grid item on the grid, it will add an image (of the **selectedNUmber**) to the clicked square
+
+### @Abdallah Elfeky:
+
+- I added lots of comments, but if you have any questions you can ask me
+- **Good luck buddy!**
--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// Getting the grid, the single squares, the select panel and the single divs inside the select panel
+
 const grid = document.querySelector(".grid");
 const gridItems = document.querySelectorAll(".gridItem");
 const select = document.querySelector(".select");
 const divs = document.querySelectorAll(".button");
+
+//declaring the state of the selected number
 let selectedNumber = 0;
 
+//creates 9x9 divs with class="gridItem" and an onclick function (displayNumber), passing itself to that function
 function displayGrid() {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
@@ -12,20 +17,24 @@ function displayGrid() {
       grid.appendChild(item);
     }
   }
-  gridItems.forEach((item) => {
-    item.addEventListener("click", displayNumber(this));
-  });
 }
 
+// handling the click on the select panel
 function handleOnClick(div, number) {
   selectedNumber = number;
+
+  //first remove the class  "selected" (-> disable the blue color) of every item
   divs.forEach((div) => {
     div.classList.remove("selected");
   });
+  //then add the class "selected" to the clicked item
   div.classList.add("selected");
-  console.log(selectedNumber);
 }
 
+//if there  is no number selected, alert an error message, else
+//set the inner HTMl of that item to an img with some styles(width, heigth) and a path
+//Note: If you want to add more pictures, you have to name them "NUMBER.jpg", replacing the NUMBER with the number shown in the picture
+// else it won't work
 function displayNumber(item) {
   if (selectedNumber === 0) {
     alert("Please choose a number!");
