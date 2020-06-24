ddd324084e978fd4beae2177ea1c588c7d17139d
Kim Logoshenko <kim.logoshenko@gmx.de>
2020-06-24 06:23:48 +0200

    added check rows and cols, checksquares is not finished yet

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const gridItems = document.querySelectorAll(".gridItem");
 const select = document.querySelector(".select");
 const divs = document.querySelectorAll(".button");
 
+let items = [];
+for (let i = 0; i < 9; i++) {
+  items.push([
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+  ]);
+}
+
 //declaring the state of the selected number
 let selectedNumber = 0;
 
@@ -13,7 +28,9 @@ function displayGrid() {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       let item = document.createElement("div");
-      item.innerHTML = `<div class="gridItem" onclick="displayNumber(this)"></div>`;
+      let id = `${i}${j}`;
+      item.innerHTML = `<div id=${id} class="gridItem" onclick="displayNumber(this)"></div>`;
+      items[i][j] = item;
       grid.appendChild(item);
     }
   }
@@ -42,9 +59,174 @@ function displayNumber(item) {
     if (selectedNumber === 10) {
       item.innerHTML = "";
     } else {
-      item.innerHTML = `<img width=50px height=50px src="images/${selectedNumber}.jpg"/>`;
+      item.innerHTML = `<img width=50px height=50px class="img${selectedNumber}" src="images/${selectedNumber}.jpg"/>`;
+      if (check(item, selectedNumber)) {
+        item.innerHTML = `<img width=50px height=50px class="img${selectedNumber}" src="images/${selectedNumber}.jpg"/>`;
+      } else {
+        item.innerHTML = "";
+        alert(
+          "That number is not allowed to go in there! Please choose another one!"
+        );
+      }
     }
   }
+}
+
+function check(item) {
+  let a = checkRow(item);
+  let b = checkCol(item);
+  let c = checkSquare(item);
+
+  if (a === true && b === true && c === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkRow(item) {
+  pos = item.id.split("");
+
+  let itemImage = document
+    .getElementById(item.id)
+    .getElementsByTagName("img")[0].src;
+  let imageURL = itemImage.substr(itemImage.length - 5);
+  let itemNumber = imageURL.substr(0, 1);
+  targets = [];
+
+  for (let i = 0; i < 9; i++) {
+    if (i === parseInt(pos[1])) {
+      continue;
+    } else {
+      target = items[pos[0]][i]
+        .getElementsByTagName("div")[0]
+        .getElementsByTagName("img")[0];
+
+      if (target !== undefined) {
+        let img = target.src.substr(target.src.length - 5);
+        let number = img.substr(0, 1);
+        targets.push(number);
+      }
+    }
+  }
+
+  for (let i = 0; i < targets.length; i++) {
+    if (itemNumber === targets[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkCol(item) {
+  pos = item.id.split("");
+
+  let itemImage = document
+    .getElementById(item.id)
+    .getElementsByTagName("img")[0].src;
+  let imageURL = itemImage.substr(itemImage.length - 5);
+  let itemNumber = imageURL.substr(0, 1);
+  targets = [];
+
+  for (let i = 0; i < 9; i++) {
+    if (i === parseInt(pos[0])) {
+      continue;
+    } else {
+      target = items[i][pos[1]]
+        .getElementsByTagName("div")[0]
+        .getElementsByTagName("img")[0];
+
+      if (target !== undefined) {
+        let img = target.src.substr(target.src.length - 5);
+        let number = img.substr(0, 1);
+        targets.push(number);
+      }
+    }
+  }
+
+  for (let i = 0; i < targets.length; i++) {
+    if (itemNumber === targets[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkSquare(item, selectedNumber) {
+  pos = item.id.split("");
+
+  let itemImage = document
+    .getElementById(item.id)
+    .getElementsByTagName("img")[0].src;
+  let imageURL = itemImage.substr(itemImage.length - 5);
+  let itemNumber = imageURL.substr(0, 1);
+  targets = [];
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {}
+  }
+
+  square = getSquare(pos[0], pos[1]);
+  console.log(square);
+
+  for (let i = 0; i < targets.length; i++) {
+    if (itemNumber === targets[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function getSquare(y, x) {
+  let coords = [];
+
+  for (let j = 0; j < 9; j += 3) {
+    for (let i = 0; i < 9; i += 3) {
+      s = [];
+      s.push(
+        [i, j],
+        [i, j + 1],
+        [i, j + 2],
+        [i + 1, j],
+        [i + 1, j + 1],
+        [i + 1, j + 2],
+        [i + 2, j],
+        [i + 2, j + 1],
+        [i + 2, j + 2]
+      );
+      coords.push(s);
+    }
+  }
+
+  console.log(x, y);
+  console.log(coords);
+  console.log(coords[3][1]);
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      console.log(coords[i][j]);
+      console.log(coords[3][1][0] === x);
+      /// HERE FIX THE CONDITIONAL
+      if (y === coords[i][j][0] && x === coords[i][j][1]) {
+        alert("hit");
+        return coords[i];
+      }
+    }
+  }
+}
+
+function getNumber(x, y) {
+  let itemNumber;
+  let coords = String(x) + String(y);
+  let itemImage = document
+    .getElementById(coords)
+    .getElementsByTagName("img")[0];
+  if (itemImage !== undefined) {
+    itemNumber = itemImage.src.substr(itemImage.src.length - 5);
+    itemNumber = itemNumber.substr(0, 1);
+  }
+  return itemNumber;
 }
 
 displayGrid();
